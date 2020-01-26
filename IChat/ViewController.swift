0675d419ef6c3edcf053f873a72d414d888f9d6f
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-01-26 14:16:55 +0300

    01. Delete Main.Storyboard and setup Canvas mode

--- IChat/ViewController.swift
+++ IChat/ViewController.swift
@@ -13,8 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
     }
 
 
+}
+
+
+import SwiftUI
+
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = ViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ViewControllerProvider.ContainerView>) -> ViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: ViewControllerProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ViewControllerProvider.ContainerView>) {
+            
+        }
+    }
 }
 
