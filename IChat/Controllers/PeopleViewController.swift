92be5c8a829c42a561b8ec5861d78f3b3d7c550d
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-01-29 01:12:06 +0300

    21. Create people filter search

--- IChat/Controllers/PeopleViewController.swift
+++ IChat/Controllers/PeopleViewController.swift
@@ -30,7 +30,7 @@ class PeopleViewController: UIViewController {
         setupSearchBar()
         setupCollectionView()
         createDataSource()
-        reloadData()
+        reloadData(with: nil)
     }
     
     private func setupCollectionView() {
@@ -55,10 +55,14 @@ class PeopleViewController: UIViewController {
         searchController.searchBar.delegate = self
     }
     
-    private func reloadData() {
+    private func reloadData(with searchText: String?) {
+        let filtered = users.filter { (user) -> Bool in
+            user.contains(filter: searchText)
+        }
+        
         var snapshot = NSDiffableDataSourceSnapshot<Section, MUser>()
         snapshot.appendSections([.users])
-        snapshot.appendItems(users, toSection: .users)
+        snapshot.appendItems(filtered, toSection: .users)
 
         dataSource?.apply(snapshot, animatingDifferences: true)
     }
@@ -144,7 +148,7 @@ extension PeopleViewController {
 // MARK: - UISearchBarDelegate
 extension PeopleViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+        reloadData(with: searchText)
     }
 }
 
